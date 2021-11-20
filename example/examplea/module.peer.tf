f6f4e30fd2fe3ef5f19267566bdf04b488d58493
James Woolfenden <James.Woolfenden@gmail.com>
2021-11-20 17:08:05 +0000

    update

--- example/examplea/module.peer.tf
+++ example/examplea/module.peer.tf
@@ -6,10 +6,10 @@ module "peer" {
     aws.requester = aws.requester
   }
   accepter_filter_name  = "tag:Name"
-  accepter_filter_value = "TEST"
+  accepter_filter_value = "withipv6"
 
-  requester_filter_name  = "tag:AccountType"
-  requester_filter_value = "Management"
+  requester_filter_name  = "tag:Name"
+  requester_filter_value = "TEST"
 
   route_table_tag_acceptor  = "TEST-Public"
   route_table_tag_requestor = "TEST-Public"
